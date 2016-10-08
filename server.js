6370c89021375820e1015211d01b4e83e8873fd8
Jinto <jinto@arvindinternet.com>
2016-10-08 10:15:48 +0530

    setting up source map

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.use(express.static('dist'));
 
+app.get('/main.min.js.map', function(req, res) {
+		res.sendFile(__dirname + '/dist/scripts/main.min.js.map');
+});
+
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
